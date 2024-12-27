8ba996b9ecd9e88ee98a8fb7aca094dee40de0ad
Antoine Toulme <antoine@lunar-ocean.com>
2024-12-27 15:37:10 -0800

    simplify

--- generate_keys.sh
+++ generate_keys.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # This script is used to generate a private key, a CA, a CSR, and a CRT for the webhook service.
-openssl genpkey -outform PEM -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out test/operator.key
-openssl req -new -nodes -key test/operator.key -config csrconfig.txt -nameopt utf8 -utf8 -out test/operator.csr
-openssl x509 -req -sha256 -days 10000 -in test/operator.csr -signkey test/operator.key -out test/operator.crt -copy_extensions=copyall
+openssl genpkey -outform PEM -algorithm RSA -pkeyopt rsa_keygen_bits:2048 -out certs/operator.key
+openssl req -new -nodes -key certs/operator.key -config certs/csrconfig.txt -nameopt utf8 -utf8 -out certs/operator.csr
+openssl x509 -req -sha256 -days 10000 -in certs/operator.csr -signkey certs/operator.key -out certs/operator.crt -copy_extensions=copyall
 echo <-DOC
 Run:
-$> cat test/operator.csr | base64 | tr -d '\n' | pbcopy
+$> cat certs/operator.csr | base64 | tr -d '\n' | pbcopy
 Paste into certs.yaml in the request field.
-Copy the test/operator.key and test/operator.crt into certs.yaml tls.key and tls.cert fields of the configmap.
+Copy the certs/operator.key and certs/operator.crt into certs.yaml tls.key and tls.cert fields of the configmap.
 Run:
-$> cat test/operator.crt | base64 | tr -d '\n' | pbcopy
+$> cat certs/operator.crt | base64 | tr -d '\n' | pbcopy
 Paste into operator.yaml under caBundle field.
 DOC
